c04d1f9b03964e5e92a22fc3e203473f5cd419af
Somechandra Sen <somechandra.sen@CaprusIT.local>
2019-12-09 23:51:47 +0530

    Updates

---
+++ precache-manifest.c677e9b1e540d1409d81bad54e0c97e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a48b5c38fe49528faca32e9db8e335",
+    "url": "/findingfalcone/index.html"
+  },
+  {
+    "revision": "fc86789ce72cfe924ca0",
+    "url": "/findingfalcone/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "43da9833fcca6aefd936",
+    "url": "/findingfalcone/static/css/main.1343b148.chunk.css"
+  },
+  {
+    "revision": "fc86789ce72cfe924ca0",
+    "url": "/findingfalcone/static/js/2.a4fab3ff.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/findingfalcone/static/js/2.a4fab3ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43da9833fcca6aefd936",
+    "url": "/findingfalcone/static/js/main.10cfe8b1.chunk.js"
+  },
+  {
+    "revision": "66754f2ce29b7a5865e6",
+    "url": "/findingfalcone/static/js/runtime-main.fab5fd82.js"
+  },
+  {
+    "revision": "56bda2c7b3385851c2bb06550f7cc7d5",
+    "url": "/findingfalcone/static/media/space-shuttle.56bda2c7.png"
+  }
+]);
